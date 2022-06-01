4ed7dc9b05ee49fd5f7885d8166048cba017e996
Cindysotog <csotogg@unah.hn>
2022-06-01 11:02:31 -0600

    main

---
+++ Ejercicios/Ejericio-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string nombre;
+
+    std::cout<<" Ingrese su nombre :";
+    cin>>nombre;
+    
+    cout<<endl;
+    cout<<"Hola mi nombre es "<<nombre;
+    cout<< endl;
+    
+    return 0;
+}
