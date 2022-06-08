dc1569873f858788e0fc2c3b1af83e97b3d04d02
Cindysotog <csotogg@unah.hn>
2022-06-08 12:46:44 -0600

    main

--- Ejercicios/Ejercicio8-Factura con Descuento/main.cpp
+++ Ejercicios/Ejercicio8-Factura con Descuento/main.cpp
@@ -2,6 +2,34 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	return 0;
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    //Datos de entrada
+    double subtotal = 0;
+    double total = 0;
+    double impuesto = 0.15;
+    int descuento = 0;
+    double calculoDescuento = 0;
+    double calculoImpuesto = 0;
+
+    cout<<"Ingrese el subtotal de la factura: ";
+    cin>> subtotal;
+    
+    cout<<"Ingrese el descuento (0, 10, 15, 20):";
+    cin>>descuento;
+
+    //Proceso
+	calculoDescuento =( subtotal * descuento) / 100;
+	calculoImpuesto = (subtotal - calculoDescuento) * 0.15;
+    total = subtotal - calculoDescuento + calculoImpuesto;
+
+    //Salida
+
+    cout<<"\nTotal a  pagar es:"<<total;
+
+    return 0;
 }
+
+
