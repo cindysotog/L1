d3bb925e662a04ef8b8aef5a3df621e0df4b1814
Cindysotog <csotogg@unah.hn>
2022-06-22 14:42:22 -0600

    main

---
+++ Ejercicios/Ejercicio17-Ciclo While Infinito/cicloinfinito.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	int ciclos = 0;
+	
+	while ( ciclos < 10000 )
+	{
+		cout << "*";
+
+		ciclos = ciclos + 1;
+		
+	}
+	
+	
+	return 0;
+}
