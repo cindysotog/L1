b09a48fd5e5590da788332dfb43d7f904535c17e
Cindysotog <csotogg@unah.hn>
2022-06-22 16:10:16 -0600

    agregando ejercicio for con variantes

---
+++ Ejercicios/Ejercicio22-Ciclos For/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	for (int indice =0; indice < 10; indice ++)
+	{
+		cout << " Valor de la variable indice " << indice << endl;
+	}
+	
+	int a = 0;
+	
+//	a = a +1;
+//	a++;
+//	a--;
+//	a**;
+//	a+-;
+	
+	return 0;
+}
