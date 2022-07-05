e970c3362de00f0df4e4db731215c0dfe65b210a
Cindysotog <csotogg@unah.hn>
2022-07-05 16:40:21 -0600

    Agregando ejercicio

---
+++ Ejercicios/Ejercicio46-Random/random.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numero = 0;
+
+
+// Inicializa el numero random
+    srand (time (NULL));
+
+    for (int i = 0; i < 20; i++)
+    {
+        // Genera un numero entre 1 y 10
+        numero = rand () % 10 + 1;
+
+        cout << "Numero al azar: " << numero << endl;
+    }
+    
+
+
+    return 0;
+}
