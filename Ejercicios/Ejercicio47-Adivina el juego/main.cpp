1ae9f59d02b3111bbc83c4ae7eb5b37a98b83b35
Cindysotog <csotogg@unah.hn>
2022-07-05 23:49:24 -0600

    Agregando ejercicio

---
+++ Ejercicios/Ejercicio47-Adivina el juego/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdlib.h>  // srand, rand
+#include <time.h>
+
+using namespace std;
+
+int main ( int argc, char const *argv[])
+{
+    int numeroSecreto = 0;
+    int miNumero = 0;
+
+    // inicializa el numero random
+    srand (time(NULL));
+
+    // genera el numero random
+    numeroSecreto = rand () % 10 + 1;
+
+    do
+    {
+        cout << "Adina el numero (1 a 10): ";
+        cin << miNumero;
+
+        if (numeroSecreto < miNumero)
+        {
+            cout << "El numero secreto puede ser menor: " << endl;
+        }
+        else
+        {
+            if (numeroSecreto > miNumero)
+            {
+                cout << "El numero secreto puede ser mayor: " << endl;
+            }
+        }
+    } while (numeroSecreto != miNumero);
+
+    cout << endl;
+    cout << endl;
+    cout << "Adivinaste!";
+
+    return 0;
+}
