3a9913a0dfcae88623f60d69bf54fb60ab1e0ee3
Cindysotog <csotogg@unah.hn>
2022-07-19 14:25:09 -0600

    Agregando ejercicio de tarea 8

---
+++ Ejercicios/Ejercicio51- Raiz Cuadrada con Math.h/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{
+    double Numero1 = 4;
+    double Numero2 = 0;
+
+    Numero2 = sqrt (Numero1);
+
+    cout << "El numero " << Numero1 << ",tiene como raiz cuadrada: " << Numero2 << endl;
+
+    return 0;
+}
