df278c6079f3ba85296dda9d52b72097303613bd
Cindysotog <csotogg@unah.hn>
2022-08-01 17:00:58 -0600

    main

---
+++ Ejercicios/Ejercicio53- Arreglos Unidemencionales/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{   
+    // Declaracion del Arreglo
+    int numeros [5];
+    
+    // Asignacion de valores a un arreglo unidimencional
+    numeros [0] = 10;
+    numeros [1] = 15;
+    numeros [2] = 20;
+    numeros [3] = 25;
+    numeros [4] = 30;
+    
+    // Lectura de los valores de un arreglo unidimencional (Vector)
+    cout << numeros [0] << endl;
+    cout << numeros [1] << endl;
+    cout << numeros [2] << endl;
+    cout << numeros [3] << endl;
+    cout << numeros [4] << endl;
+    
+    return 0;
+}
